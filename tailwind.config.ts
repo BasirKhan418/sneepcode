e8cdfb836f0e647cbf609dea9fcaeccebb011437
Aniket Subudhi <aniketsubudhi00@gmail.com>
2025-07-24 15:04:51 +0530

    new changes

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config;
